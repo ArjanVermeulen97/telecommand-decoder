3f42f1c82664b35972811592790e04c5ab45ac1a
Arjan Vermeulen <Anterzhul@yahoo.com>
2020-08-30 16:05:52 +0200

    wrote new main code for processing input slightly more elegantly

--- main/main.ino
+++ main/main.ino
@@ -1,73 +1,97 @@
-char incoming_value = 0;
-int acq_seq_count = 0;
-int LED_PIN_ACQ = 11;
-int LED_PIN_START = 12;
-int LED_PIN_CONN = 13;
+byte acq_buffer;
+byte last_acq_bit = 1;
+int start_seq_counter = 0;
+
+long START_SEQUENCE = 0xEB90;
+long start_buffer = 0;
+
+byte recv_buffer[8];
+int recv_bytes = 0;
+
 int state = 0;
-char start_seq_buffer[16] = {""};
-char start_seq[16] = {0b1, 0b1, 0b1, 0b0, 0b1, 0b0, 0b1, 0b1, 0b1, 0b0, 0b0, 0b1, 0b0, 0b0, 0b0, 0b0};
-int start_seq_error = 0;
+int STATE_ACQ_LED = 2;
+int STATE_START_LED = 4;
+int STATE_RECV_LED = 6;
+
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_PIN_ACQ, OUTPUT);
-  pinMode(LED_PIN_START, OUTPUT);
-  pinMode(LED_PIN_CONN, OUTPUT);
-  digitalWrite(LED_PIN_ACQ, HIGH);
-  digitalWrite(LED_PIN_START, LOW);
-  digitalWrite(LED_PIN_CONN, LOW);
+  pinMode(STATE_ACQ_LED, OUTPUT);
+  pinMode(STATE_START_LED, OUTPUT);
+  pinMode(STATE_RECV_LED, OUTPUT);
+  digitalWrite(STATE_ACQ_LED, HIGH);
+  digitalWrite(STATE_START_LED, LOW);
+  digitalWrite(STATE_RECV_LED, LOW);
+  Serial.println("<Receiver is ready>");
 }
 
 void loop() {
-  if (state == 0){
-    //Looking for acquisition sequence
-    if(Serial.available() > 0) {
-      incoming_value = Serial.read();
-      // Read back incoming value.
-      Serial.print(incoming_value);
-      Serial.print("\n");
-      // TO DO: Make it count alternating 1's and 0's.
-      acq_seq_count++;
-    }
-    if (acq_seq_count == 64) {
-      // Go to next state, search for start sequence
-      Serial.print("Acquisition sequence passed, state 1\n");
-      state = 1;
-      digitalWrite(LED_PIN_ACQ, LOW);
-      digitalWrite(LED_PIN_START, HIGH);
-    }
-  }
-
-  if (state == 1){
-    // Looking for EB90 sequence
-    // Reset the variable used to count the start sequence data buffer
-    start_seq_error = 0;
+  // put your main code here, to run repeatedly:
+  if (state == 0) {
     if (Serial.available() > 0) {
-      for (int i=0; i<16; i++){
-        // Shift all entries in the buffer one place to the left
-        // TO DO: Implement circular buffer? This is very inefficient
-        start_seq_buffer[i] = start_seq_buffer[i+1];
+      acq_buffer = recvBin();
+      if (acq_buffer ^ last_acq_bit == B1){
+        start_seq_counter += 1;
+        last_acq_bit = acq_buffer;
       }
-      // Read ASCII data ("0" = 48, "1" = 49) into buffer as binary 0 and 1.
-      start_seq_buffer[15] = Serial.read() - 48;
-      for (int i=0; i<16; i++) {
-        // Print back content of buffer for debugging purposes
-        Serial.print(start_seq_buffer[i], BIN);
+      if (last_acq_bit == 1 && start_seq_counter >= 64) {
+        Serial.println("<Acquisition sequence detected>");
+        state = 1;
+        digitalWrite(STATE_START_LED, HIGH);
+        digitalWrite(STATE_ACQ_LED, LOW);
       }
-      Serial.print("\n");
-      for (int i=0; i<16; i++) {
-        // Count entries and compare to reference sequence.
-        if (start_seq_buffer[i] != start_seq[i]) {
-          start_seq_error++;
+    }
+  } else if (state == 1) {
+    // Receive new bit into start_buffer
+    if (Serial.available() > 0) {
+      start_buffer = start_buffer << 1;
+      start_buffer = start_buffer + recvBin();
+      start_buffer = start_buffer % 65536;
+      if (start_buffer == START_SEQUENCE) {
+        Serial.println("<Start sequence detected>");
+        state = 2;
+        digitalWrite(STATE_RECV_LED, HIGH);
+        digitalWrite(STATE_START_LED, LOW);
+      } else {
+        // Check if there's a single error, then approve anyway
+        for (int i=0; i<16; i++) {
+          long error_start_buffer = start_buffer ^ (B1 << i);
+          if (error_start_buffer == START_SEQUENCE) {
+            Serial.println("<Start sequence detected>");
+            Serial.println("-- One error detected and corrected in start sequence --");
+            state = 2;
+            digitalWrite(STATE_RECV_LED, HIGH);
+            digitalWrite(STATE_START_LED, LOW);
+          }
         }
       }
-      if (start_seq_error == 1 | start_seq_error == 0) {
-        // If 0 or 1 error, go to state 2
-        state = 2;
-        Serial.print("Start sequence passed, state 2\n");
-        digitalWrite(LED_PIN_START, LOW);
-        digitalWrite(LED_PIN_CONN, HIGH);
+    }
+  } else if (state == 2) {
+    // Receive octets
+    if (Serial.available() >= 8) {
+      for (int i=0; i<8; i++) {
+        if (i == 0){
+          recv_buffer[recv_bytes] = 0;
+        }
+        recv_buffer[recv_bytes] = recv_buffer[recv_bytes] + (recvBin() << (7 - i));
+      }
+      // Increment the amount of octets we received already
+      recv_bytes++;
+      if (recv_bytes >= 8) {
+        // Reset counter
+        recv_bytes = recv_bytes % 8;
+        // Process CLTU HERE
+        Serial.println("<Received CLTU>");
+        for (int i=0; i<8; i++) {
+          Serial.println(recv_buffer[i], BIN);
+        }
       }
     }
   }
+}
+
+byte recvBin() {
+  byte recv_buffer = Serial.read() - 48;
+  return recv_buffer;
 }
